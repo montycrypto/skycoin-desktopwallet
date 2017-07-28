6aed3dc9963ced33913bc329173cafe7ff899ca5
Unknown <xano3425@gmail.com>
2017-07-28 12:05:55 +0200

    Add tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MdCardModule, MdIconModule, MdListModule, MdTabsModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
