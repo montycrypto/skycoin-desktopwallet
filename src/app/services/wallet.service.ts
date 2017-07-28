8458f88b267724e9895fdeb578d1d755e5baa745
Unknown <xano3425@gmail.com>
2017-07-28 13:48:54 +0200

    Add loadData

--- src/app/services/wallet.service.ts
+++ src/app/services/wallet.service.ts
@@ -4,6 +4,8 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Subject } from 'rxjs/Subject';
 import { WalletModel } from '../models/wallet.model';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/forkJoin';
+import 'rxjs/add/operator/first';
 
 @Injectable()
 export class WalletService {
@@ -13,33 +15,25 @@ export class WalletService {
   constructor(
     private apiService: ApiService
   ) {
-    this.retrieveWallets();
+    this.loadData();
   }
 
   all(): Observable<WalletModel[]> {
     return this.wallets.asObservable();
   }
 
-  private retrieveWallets() {
-    this.apiService.get('wallets').subscribe(data => {
-      console.log(data);
-    });
-    // return Observable.fromPromise(this.file.listDir(this.file.externalRootDirectory, 'superwallet'))
-    //   .map(paths => paths.filter(path => path.name.substr(path.name.length - 4) === '.wlt'))
-    //   .flatMap(paths => {
-    //     const files = paths.map(path => this.file.readAsText(this.file.externalRootDirectory, 'superwallet/' + path.name));
-    //     return Observable.forkJoin(files).map(files => files.map(file => {
-    //       let wallet = JSON.parse(file);
-    //       wallet.balance = -1;
-    //       return wallet;
-    //     }));
-    //   });
   }
 
   private loadData(): void {
-    // this.indexWallets().first().subscribe(wallets => {
-    //   this.wallets.next(wallets);
-    //   this.refreshBalances();
-    // });
+    this.retrieveWallets().first().subscribe(wallets => {
+      this.wallets.next(wallets);
+      this.refreshBalances();
+    });
+  }
+
+  private refreshBalances() {
+  }
+  private retrieveWallets(): Observable<WalletModel[]> {
+    return this.apiService.get('wallets');
   }
 }
