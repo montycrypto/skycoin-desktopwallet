c4d3c97deb230b374b54d97579b720b109941f03
Unknown <xano3425@gmail.com>
2017-07-28 20:54:14 +0200

    Rename address detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {
-  MdButtonModule, MdCardModule, MdDialogModule, MdExpansionModule, MdIconModule, MdInputModule, MdListModule,
-  MdTabsModule
-} from '@angular/material';
+import { MdButtonModule, MdCardModule, MdDialogModule, MdExpansionModule, MdIconModule, MdInputModule, MdListModule, MdTabsModule } from '@angular/material';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 import { ApiService } from './services/api.service';
 import { WalletService } from './services/wallet.service';
 import { WalletsComponent } from './components/pages/wallets/wallets.component';
-import { AddressDetailComponent } from './components/pages/wallets/address-detail/address-detail.component';
+import { WalletDetailComponent } from './components/pages/wallets/address-detail/wallet-detail.component';
 import { CreateWalletComponent } from './components/pages/wallets/create-wallet/create-wallet.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -19,7 +15,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   declarations: [
     AppComponent,
     WalletsComponent,
-    AddressDetailComponent,
+    WalletDetailComponent,
     CreateWalletComponent,
   ],
   entryComponents: [
