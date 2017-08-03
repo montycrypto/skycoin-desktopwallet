3d128c970001b7d93e64e12341ba6cdf9dca40e7
Unknown <xano3425@gmail.com>
2017-08-03 23:15:04 +0200

    Add transaction detail to block detail

--- src/app/services/blockchain.service.ts
+++ src/app/services/blockchain.service.ts
@@ -10,10 +10,26 @@ export class BlockchainService {
   ) { }
 
   block(id): Observable<any> {
-    return this.apiService.get('blocks', { start: id, end: id }).map(response => response.blocks[0]);
+    return this.apiService.get('blocks', { start: id, end: id }).map(response => response.blocks[0]).flatMap(block => {
+      return Observable.forkJoin(block.body.txns.map(transaction => {
+        return Observable.forkJoin(transaction.inputs.map(input => this.retrieveInputAddress(input).map(response => {
+          return response.owner_address;
+        }))).map(inputs => {
+          transaction.inputs = inputs;
+          return transaction;
+        });
+      })).map(transactions => {
+        block.body.txns = transactions;
+        return block;
+      });
+    });
   }
 
   blocks() {
     return this.apiService.get('last_blocks', { num: 100 }).map(response => response.blocks.reverse());
+  }
+
+  private retrieveInputAddress(input: string) {
+    return this.apiService.get('uxout', {uxid: input});
   }
 }
